5e4a8e26858af5ebdbfb42eea7fbb1c04a4e0753
mbarbari <mbarbari@student.42.fr>
2015-11-01 21:09:55 +0100

    quelques fonctions utiles pour les vertex dont la somme avec un entier utilise dans les calculs des objets

--- include/framework/fk_vertexiel.h
+++ include/framework/fk_vertexiel.h
@@ -6,7 +6,7 @@
 /*   By: mbarbari <mbarbari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/29 21:18:55 by mbarbari          #+#    #+#             */
-/*   Updated: 2015/10/29 22:53:53 by mbarbari         ###   ########.fr       */
+/*   Updated: 2015/11/01 21:08:17 by mbarbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ struct			s_vertex3
 	float z;
 };
 
-t_vertex3		new_vertex(float x1, float y1, float z1);
+t_vertex3		vertex_new(float x1, float y1, float z1);
 t_vertex3		vertex_sum(t_vertex3 u, unsigned int magnitude);
+unsigned int	lenght_segment(t_vertex3 u, t_vertex3 v);
 
 #endif
---
+++ src/framework_collision/fk_vertex_math.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fk_vertex_math.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarbari <mbarbari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/01 19:21:55 by mbarbari          #+#    #+#             */
+/*   Updated: 2015/11/01 21:08:10 by mbarbari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fk_vertexiel.h>
+#include <fk_math.h>
+
+unsigned int		lenght_segment(t_vertex3 u, t_vertex3 v)
+{
+	return (sqrt(SQUARE(u.x - v.x) + SQUARE(u.y - v.y)  + SQUARE(u.z - v.z) ));
+}
+
+t_vertex3			vertex_sum(t_vertex3 u, unsigned int l)
+{
+	return ((t_vertex3) {.x = u.x + l, .y = u.y + l, .z = u.z + l});
+}
+
+t_vertex3		vertex_new(float x1, float y1, float z1)
+{
+	return ((t_vertex3) {.x = x1, .y = y1, .z = z1});
+}
