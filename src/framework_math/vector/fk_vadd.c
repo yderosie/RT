32e9724eefb4c02ff64819adf1ad76f9184bd5e7
mbarbari <mbarbari@student.42.fr>
2016-02-04 22:25:40 +0100

    vector function add, mul, sub

--- src/framework_math/vector/fk_vadd.c
+++ src/framework_math/vector/fk_vadd.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fk_vsub.c                                          :+:      :+:    :+:   */
+/*   fk_vadd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roblabla <rlambert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: barbare <barbare@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/28 19:22:26 by roblabla          #+#    #+#             */
-/*   Updated: 2016/02/04 14:40:40 by barbare          ###   ########.fr       */
+/*   Created: 2016/02/04 14:42:32 by barbare           #+#    #+#             */
+/*   Updated: 2016/02/04 14:44:22 by barbare          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "framework_math/fk_vector.h"
 
-t_vector3		vector_sub(t_vector3 u, float r)
+t_vector3		vector_add(t_vector3 u, float r)
 {
-	return ((t_vector3) { .x = u.x - r, .y = u.y - r, .z = u.z - r });
+	return ((t_vector3) { .x = u.x + r, .y = u.y + r, .z = u.z + r });
 }
 
