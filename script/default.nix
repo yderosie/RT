f499985688baac15f0b69f1335cd92c3162fec81
yderosie <yderosie@student.42.fr>
2016-04-04 21:13:21 +0200

    clean

--- script/default.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-  stdenv = pkgs.stdenv;
-in
-  stdenv.mkDerivation {
-    name = "42-env-1.0.0";
-    version = "1.0.0";
-    buildInputs = [ pkgs.x11 pkgs.pkgconfig ];
-  }
